de8c80e4e6b9e31bd8a2152b30fdce0912aa5516
Rubaid07 <rubaidindu@gmail.com>
2025-08-02 13:21:17 +0600

    added user register route

--- index.js
+++ index.js
@@ -18,12 +18,33 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
+let db, usersCollection, recipesCollection;
 async function run() {
   try {
     await client.connect();
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    
+    db = client.db("plateGenieDB");
+    usersCollection = db.collection("users");
+    recipesCollection = db.collection("recipes");
+
+    // ---------------USER MANAGEMENT ROUTE---------------
+    app.post('/api/register', async (req, res) => {
+      const { username, email, password } = req.body;
+      if (!username || !email || !password) {
+        return res.status(400).json({ message: 'All fields are required.' });
+      }
+      try {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        const user = { username, email, password: hashedPassword };
+        const result = await usersCollection.insertOne(user);
+        res.status(201).json({ message: 'User registered successfully!' });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error.' });
+      }
+    });
   } finally {
 
   }
